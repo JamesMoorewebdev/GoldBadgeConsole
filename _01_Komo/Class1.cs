08c89cc52f7a1da53b1a0973041192a414e4566a
JamesMoorewebdev <72464374+JamesMoorewebdev@users.noreply.github.com>
2020-10-07 07:11:16 -0700

    Add files via upload

---
+++ _01_Komo/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Komo
+{
+    public enum FoodTypeDay
+    {
+        Breakfast = 1,
+        Lunch,
+        Dinner
+    }
+    public class Options
+    {
+        public FoodTypeDay Type { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        
+        public decimal Price { get; set; }
+        public bool DealBuy
+        {
+            get
+            {
+                if (Price >= 20)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public string DisplayIngdnts(List<string> listOfIngdnts)
+        {
+            string ingredients = string.Join(", ", listOfIngdnts);
+            return ingredients;
+        }
+        public Options()
+        {
+
+        }
+        public Options(FoodTypeDay foodType, string name, int number, string description, List<string> ingredients, decimal price)
+        {
+            Type = foodType;
+            Name = name;
+            Number = number;
+            Description = description;
+            Ingredients = ingredients;
+            Price = price;
+        }
+
+    }
+}
---
+++ _01_Komo/MealRepo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Komo
+{
+    public class FoodRepository
+    {
+        protected readonly List<Options> _allMeals = new List<Options>();
+        public bool AddMealsToMenu(Options meal)
+        {
+            int startingCount = _allMeals.Count;
+            _allMeals.Add(meal);
+            bool wasAdded = (_allMeals.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+
+        public bool DeleteExistingMenuItem(Options existingMeal)
+        {
+            bool wasDeleted = _allMeals.Remove(existingMeal);
+            return wasDeleted;
+        }
+
+        public List<Options> GetItemsFromMenu()
+        {
+            return _allMeals;
+        }
+
+        public Options GetMealByName(string name)
+        {
+            foreach (Options meal in _allMeals)
+            {
+                if (meal.Name.ToLower() == name.ToLower())
+                {
+                    return meal;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, response not recognized.");
+                }
+            }
+            return null;
+        }
+
+    }
+
+}
